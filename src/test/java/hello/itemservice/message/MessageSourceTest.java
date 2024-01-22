3236b0f6a76bcee002e9ea4f9e05e57eb1a85ddd
hyunjin_byun <shinbm219@naver.com>
2024-01-22 15:35:17 +0900

    [섹션3]메세지, 국제화:스프링 메세지 소스 사용

--- src/test/java/hello/itemservice/message/MessageSourceTest.java
+++ src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -1,6 +1,7 @@
 package hello.itemservice.message;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,40 +16,46 @@ import static org.assertj.core.api.Assertions.*;
 public class MessageSourceTest {
 
     @Autowired
-    MessageSource ms;
+    MessageSource messageSource;
 
     @Test
     void helloMessage() {
-        String result = ms.getMessage("hello", null, null);
+        String result = messageSource.getMessage("hello", null, null);
+        // locale이 따로 없으면 basename에서 설정한 디폴트 값인 messages.properties가 선택
         assertThat(result).isEqualTo("안녕");
     }
 
+    // no_code라는 메세지가 설정이 없다.
+    @DisplayName("값이 없는 경우")
     @Test
     void notFoundMessageCode() {
-        assertThatThrownBy(() -> ms.getMessage("no_code", null, null))
+        assertThatThrownBy(() -> messageSource.getMessage("no_code", null, null))
                 .isInstanceOf(NoSuchMessageException.class);
     }
 
+    @DisplayName("값이 없는 경우 디폴트값 설정")
     @Test
     void notFoundMessageCodeDefaultMessage() {
-        String result = ms.getMessage("no_code", null, "기본 메시지", null);
+        String result = messageSource.getMessage("no_code", null, "기본 메시지", null);
         assertThat(result).isEqualTo("기본 메시지");
     }
 
     @Test
     void argumentMessage() {
-        String message = ms.getMessage("hello.name", new Object[]{"Spring"}, null);
+        String message = messageSource.getMessage("hello.name", new Object[]{"Spring"}, null);
         assertThat(message).isEqualTo("안녕 Spring");
     }
 
+    @DisplayName("국제화")
     @Test
     void defaultLang() {
-        assertThat(ms.getMessage("hello", null, null)).isEqualTo("안녕");
-        assertThat(ms.getMessage("hello", null, Locale.KOREA)).isEqualTo("안녕");
+        assertThat(messageSource.getMessage("hello", null, null)).isEqualTo("안녕");
+        assertThat(messageSource.getMessage("hello", null, Locale.KOREA)).isEqualTo("안녕");
     }
 
+    @DisplayName("국제화_영어")
     @Test
     void enLang() {
-        assertThat(ms.getMessage("hello", null, Locale.ENGLISH)).isEqualTo("hello");
+        assertThat(messageSource.getMessage("hello", null, Locale.ENGLISH)).isEqualTo("hello");
     }
 }
